4aeba3d1cc28512ebc7f149786b91942cddd9cb2
Thiago Carvalho <tdecarv@gmail.com>
2024-07-12 17:41:23 -0400

    Update to jsx extension

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import Socials from './Socials';
+
+function HomePage() {
+  return (
+    <div className="HomePage-container">
+      <h1 className="HomePage-title">Thiago Carvalho</h1>
+      <Socials />
+    </div>
+  );
+}
+
+export default HomePage;
