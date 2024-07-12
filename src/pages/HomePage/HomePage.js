9d9ca70f319e42636a3d63c9c71ee0a3e65f6ed2
Thiago Carvalho <tdecarv@gmail.com>
2024-07-12 15:05:50 -0400

    Create HomePage page

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className='HomePage-container'>
+      <h1 className='HomePage-title'>Thiago Carvalho</h1>
+    </div>
+  );
+}
+
+export default HomePage;
