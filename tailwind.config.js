3184fb363bcb0d7bc6ef516e07d5ac1e2f3deef0
Thiago Carvalho <tdecarv@gmail.com>
2024-07-12 17:41:49 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"],
+        inter: ["Inter", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
